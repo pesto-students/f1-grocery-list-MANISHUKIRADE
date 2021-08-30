a99e0d57902b9f95642f6e898025eddd95a89d5c
MANISHUKIRADE <ukirademanish9797@gmail.com>
2021-08-31 01:20:55 +0530

    added pagination inside list app and get data from server

--- src/components/productList/productList.tsx
+++ src/components/productList/productList.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useEffect, useState, createContext } from 'react';
 import './productList.css';
-import products from '../../product.json';
+import ReactPaginate from 'react-paginate';
 import Product from '../product/product';
-
+import  fetchAllProducts  from '../../services/api'
 interface Product {
     title: string,
     type: string,
@@ -15,6 +15,18 @@ interface Product {
     rating: number,
     inCart : boolean
 }
+interface ProductList{
+  docs: Array<Product>
+  totalDocs: number
+}
+
+ async function getProduct(currentPage:number,pageLimit:number,setProduct:any,setTotalObject:any){
+  const result  = await fetchAllProducts(currentPage,pageLimit)
+  console.log(result)
+  setProduct(result.docs)
+  setTotalObject(result.totalDocs)
+  return
+}
 
 function ProductList(): any {
   const [productList, setProductList] = useState <Product[]>([]);
@@ -23,18 +35,29 @@ function ProductList(): any {
     cartItems,
     setCartItems,
   };
+  const pageLimit =10;
+  const [currentPage,setCurrentPage] = useState <number>(1);
+  const [totalObject,setTotalObject] = useState <number>(0)
   const CartContext = createContext(initialState);
+  
   useEffect(() => {
-    setProductList(products);
-  });
+    getProduct(currentPage,pageLimit,setProductList,setTotalObject)
+  },[currentPage]);
   return (
+    <>
     <div className="list">
       <CartContext.Provider value={{ cartItems, setCartItems }}>
         {
-                productList.map((product) => <Product  product={product} CartContext={CartContext} />)
-            }
+          productList.map((product) => <Product key={product.title}  product={product} CartContext={CartContext} />)
+        }
       </CartContext.Provider>
     </div>
+    <ReactPaginate pageCount={totalObject/pageLimit}  marginPagesDisplayed={2} pageRangeDisplayed={5}
+          onPageChange={(data): void =>{
+          getProduct(data.selected,pageLimit,setProductList,setTotalObject)
+          setCurrentPage(data.selected)          
+        }} ></ReactPaginate>
+    </>
   );
 }
 
